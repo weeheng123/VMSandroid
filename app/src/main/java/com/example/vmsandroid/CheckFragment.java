6a12aa65feda3b17006307a55bdd0b51cff9cf58
thaqif <thaqifs.info@gmail.com>
2021-01-07 16:53:45 +0800

    added guard registration, fix login

---
+++ app/src/main/java/com/example/vmsandroid/CheckFragment.java
@@ -0,0 +1,30 @@
+package com.example.vmsandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class CheckFragment extends Fragment {
+
+
+
+
+    public CheckFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_check, container, false);
+    }
+
+}
